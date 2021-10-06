31b7f78dcfe4e4fb10197ebb73250cfd82de755c
Taylor McKinnon <mail@tmacs.space>
2021-10-05 22:03:40 -0700

    Improve packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ import setuptools
 setuptools.setup(
     name='today',
     version='0.1.0',
+    description='Simple scratch directory and notes manager',
+    author='Taylor McKinnon',
+    author_email='mail@tmacs.space',
+    url='https://github.com/tmacro/today/',
+    packages=['today', 'today.tn', 'today.td'],
+    package_data={
+        'today': [ 'defaults.conf']
+    },
     entry_points={
         'console_scripts': [
             'tn=today.tn.entry:cli',
